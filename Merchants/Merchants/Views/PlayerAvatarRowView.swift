fb77394e5b780faae634d72b278cfc02c291235e
AD <silentflute.ad@gmail.com>
2025-08-25 20:26:37 +0100

    adding comments

--- Merchants/Merchants/Views/PlayerAvatarRowView.swift
+++ Merchants/Merchants/Views/PlayerAvatarRowView.swift
@@ -6,9 +6,9 @@
 //
 
 import SwiftUI
+import MerchantsEngine
 
 // List all avatars in a row
-
 struct PlayerAvatarRowView: View {
     var body: some View {
         Text("Avatar rows")
--- Merchants/Merchants/Views/PlayerSelectScreenView.swift
+++ Merchants/Merchants/Views/PlayerSelectScreenView.swift
@@ -7,11 +7,29 @@
 
 import SwiftUI
 import Foundation
+import MerchantsEngine
 
+// Screen to select 2-4 players
+// Uses avatars and switch AI on/off
 struct PlayerSelectScreenView: View {
-    var body: some View {
-        Text("Select players")
-            .font(.headline)
+    var body: some View {        
+        VStack {
+            Text("Select players")
+                .font(.headline)
+            
+            Button(action: {
+                print("Pressed: Play")
+                // Hook for Play action
+            }) {
+                Text("Play")
+                    .font(.title2)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue.opacity(0.85))
+                    .foregroundColor(.white)
+                    .cornerRadius(12)
+            }
+        }
     }
 }
 
