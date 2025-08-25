7b7bf62543aa39bf25eac73c07ee9cdbf64fa922
AD <silentflute.ad@gmail.com>
2025-08-25 19:26:07 +0100

    Initial project build - with art assets

---
+++ Merchants/Merchants/MerchantsApp.swift
@@ -0,0 +1,20 @@
+//
+//  MerchantsApp.swift
+//  Merchants
+//
+//  Created by Amarjit on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MerchantsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
