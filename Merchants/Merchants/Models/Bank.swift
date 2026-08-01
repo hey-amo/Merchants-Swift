1b4abc13eaf6ba5e29e6d4eba4fbe840d322e333
AD <silentflute.ad@gmail.com>
2026-08-01 08:01:45 +0100

    Updating bank, fixing the test

--- Merchants/Merchants/Models/Bank.swift
+++ Merchants/Merchants/Models/Bank.swift
@@ -3,8 +3,8 @@
 import Foundation
 
 enum IntegerErrorType: Error {
-	case negativeIntegerValue
-	case notEnoughFunds
+    case negativeIntegerValue
+    case notEnoughFunds
 }
 
 extension IntegerErrorType: CustomStringConvertible {
@@ -17,45 +17,70 @@ extension IntegerErrorType: CustomStringConvertible {
 }
 
 enum Transaction {
-	case credit(Int)
-	case debit(Int)
+    case credit(Int)
+    case debit(Int)
 }
 
-class Bank {
-	private var _balance: Int 
-    public var balance: Int { return _balance }
+class Bank: NSObject, NSCoding {
+    private var _balance: Int
+    public var balance: Int { _balance }
 
-	init(_ balance: Int = 0) {
-		self._balance = balance
-	}
+    init(_ balance: Int = 0) throws {
+        guard balance >= 0 else {
+            throw IntegerErrorType.negativeIntegerValue
+        }
+        self._balance = balance
+        super.init()
+    }
 
-	func perform(_ t: Transaction) {
-		switch t {
-        case .credit(let amount): credit(amount)
-        case .debit(let amount): debit(amount)
-		}
-	}
+    required init?(coder: NSCoder) {
+        let decodedBalance = coder.decodeInteger(forKey: "balance")
+        guard decodedBalance >= 0 else {
+            return nil
+        }
+        self._balance = decodedBalance
+        super.init()
+    }
+
+    convenience init?(withCoder coder: NSCoder) {
+        self.init(coder: coder)
+    }
+
+    func perform(_ t: Transaction) throws {
+        switch t {
+        case .credit(let amount): try credit(amount)
+        case .debit(let amount): try debit(amount)
+        }
+    }
+
+    func encode(with coder: NSCoder) {
+        coder.encode(_balance, forKey: "balance")
+    }
+
+    func save(with coder: NSCoder) {
+        encode(with: coder)
+    }
+
+    func saveWithCoder(_ coder: NSCoder) {
+        save(with: coder)
+    }
 }
 
 extension Bank {
-    private func credit(_ amount: Int = 0) {
-        guard canCredit(amount) else { return }
-        self._balance += amount
+    private func credit(_ amount: Int = 0) throws {
+        guard amount > 0 else {
+            throw IntegerErrorType.negativeIntegerValue
+        }
+        _balance += amount
     }
-    private func debit(_ amount: Int = 0) {
-        guard canDebit(amount) else { return }
-        self._balance -= amount
-    }
-}
 
-extension Bank {
-	private func canCredit(_ amount: Int = 0) -> Bool {
-		guard amount > 0 else { return false}
-		return true
-	}
-	private func canDebit(_ amount: Int = 0) -> Bool {
-        guard (amount > 0) else { return false }
-		guard (balance >= amount) else { return false }
-		return true
-	}
+    private func debit(_ amount: Int = 0) throws {
+        guard amount > 0 else {
+            throw IntegerErrorType.negativeIntegerValue
+        }
+        guard balance >= amount else {
+            throw IntegerErrorType.notEnoughFunds
+        }
+        _balance -= amount
+    }
 }
--- Merchants/MerchantsTests/BankTests.swift
+++ Merchants/MerchantsTests/BankTests.swift
@@ -10,47 +10,77 @@ import XCTest
 
 final class BankTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-    
     func testBalance() throws {
         let expected = 100
-        let bank = Bank(expected)
+        let bank = try Bank(expected)
         XCTAssertEqual(bank.balance, 100)
-    }
-    
-    func testCredit_PositiveValue() throws {
-        let expected = 100
-        let bank = Bank(0)
-        bank.perform(.credit(expected))
-        XCTAssertEqual(bank.balance, 100)
-    }
-    
-    func testCredit_NegativeValue_ShouldFail() throws {
-        
-    }
-    
-    func testDebit_PositiveValue() throws {
-        let deposit = 100
-        let subtract = 10
-        let expected = (deposit - subtract)
-        let bank = Bank(deposit)
-        XCTAssertEqual(bank.balance, deposit)
-        bank.perform(.debit(expected))
-        XCTAssertEqual(bank.balance, expected)
-    }
-    
-    func testDebit_NegativeValue_ShouldFail() throws {
-        
-    }
-    
-    func testDebit_NotEnoughFunds_ShouldFail() throws {
-        
     }
 
+    func testCredit_PositiveValue() throws {
+        let expected = 100
+        let bank = try Bank(0)
+        try bank.perform(.credit(expected))
+        XCTAssertEqual(bank.balance, 100)
+    }
+
+    func testCredit_NegativeValue_ShouldFail() throws {
+        let bank = try Bank(100)
+        XCTAssertThrowsError(try bank.perform(.credit(-10))) { error in
+            XCTAssertEqual(error as? IntegerErrorType, .negativeIntegerValue)
+        }
+    }
+
+    func testDebit_ValueIsPositive() throws {
+        let deposit = 100
+        let subtract = 10
+        let expectedBalance = deposit - subtract
+        let bank = try Bank(deposit)
+        XCTAssertEqual(bank.balance, deposit)
+        try bank.perform(.debit(subtract))
+        XCTAssertEqual(bank.balance, expectedBalance)
+    }
+
+    func testDebit_NegativeValue_ShouldFail() throws {
+        let bank = try Bank(100)
+        XCTAssertThrowsError(try bank.perform(.debit(-10))) { error in
+            XCTAssertEqual(error as? IntegerErrorType, .negativeIntegerValue)
+        }
+    }
+
+    func testDebit_NotEnoughFunds_ShouldFail() throws {
+        let bank = try Bank(10)
+        XCTAssertThrowsError(try bank.perform(.debit(20))) { error in
+            XCTAssertEqual(error as? IntegerErrorType, .notEnoughFunds)
+        }
+    }
+
+    func testInit_NegativeBalance_ShouldThrow() {
+        XCTAssertThrowsError(try Bank(-10)) { error in
+            XCTAssertEqual(error as? IntegerErrorType, .negativeIntegerValue)
+        }
+    }
+
+    func testInitWithCoder_RoundTripsBalance() throws {
+        let bank = try Bank(42)
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWith: data)
+        bank.encode(with: archiver)
+        archiver.finishEncoding()
+
+        let unarchiver = NSKeyedUnarchiver(forReadingFrom: data as Data)
+        let decodedBank = Bank(withCoder: unarchiver)
+        XCTAssertEqual(decodedBank?.balance, 42)
+    }
+
+    func testSaveWithCoder_EncodesBalance() throws {
+        let bank = try Bank(27)
+        let data = NSMutableData()
+        let archiver = NSKeyedArchiver(forWritingWith: data)
+        bank.save(with: archiver)
+        archiver.finishEncoding()
+
+        let unarchiver = NSKeyedUnarchiver(forReadingFrom: data as Data)
+        let decodedBank = Bank(withCoder: unarchiver)
+        XCTAssertEqual(decodedBank?.balance, 27)
+    }
 }
